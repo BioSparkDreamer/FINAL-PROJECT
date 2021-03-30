4480290a4ce3d62ea16711d755561529ee88c4e8
BentNai1 <eattopramen@hotmail.com>
2021-03-30 00:58:20 -0400

    Basic Projectile launching system
    The bones for a broader system; realized I should probably save it.

---
+++ ALL THAT GLOWS/Assets/Scripts/PlayerWeaponFire.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponFire : MonoBehaviour
+{
+    //......................................Variables
+    //weapons: 0Unarmed - 1Lightning - 2Fire - 3Ice
+    static public float weapon;
+    public int startingWeapon = 0;
+
+    public GameObject projectileObject;
+    public Transform fireProjectileFromLocation;
+    public Transform playerPosition;
+
+    private float shotCooldown = 0;
+
+
+   
+    void Start()
+    {
+        //weapons: 0Unarmed - 1Lightning - 2Fire - 3Ice
+        weapon = startingWeapon;
+        
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && shotCooldown <= 0)
+        {
+            //spawn projectile
+            GameObject newProjectile = Instantiate(projectileObject, fireProjectileFromLocation.position, fireProjectileFromLocation.rotation);
+
+            //calculate shoot angle
+            Vector3 shootDirection = (fireProjectileFromLocation.position - playerPosition.position);
+            //send projectile off
+            newProjectile.GetComponent<ProjectilePhysics>().ProjectileProperties(shootDirection, 5);
+            Destroy(newProjectile, 5f);
+
+            //LIGHTNING
+            if(weapon == 1)
+            {
+
+            }
+
+            //FIRE
+            if(weapon == 2)
+            {
+
+            }
+
+            //ICE
+            if(weapon == 3)
+            {
+
+            }
+
+
+        }
+    }
+}
---
+++ ALL THAT GLOWS/Assets/Scripts/ProjectilePhysics.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilePhysics : MonoBehaviour
+{
+    //...................................................................Move Projectile
+    public void ProjectileProperties(Vector3 shootDirection, float shootSpeed)
+    {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+
+        //creates a one time push in a direction, maintains that speed
+        rigidBody.AddForce(shootDirection * shootSpeed, ForceMode.Impulse);
+    }
+}
