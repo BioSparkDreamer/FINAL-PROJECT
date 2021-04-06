9b2aff32a4a5d0b330420c45a4f53e63bc8d717f
BentNai1 <eattopramen@hotmail.com>
2021-04-06 02:24:28 -0400

    Re-tooled spawner, missing movement

---
+++ ALL THAT GLOWS/Assets/Scripts/EnemyMovementPath.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementPath : MonoBehaviour
+{
+    [Header("Put waypoints below (ending at castle).")]
+    [Tooltip("This 'script' just passes the waypoints you put into here off into the SpawnerOfEnemiesScript.")]
+    public GameObject[] wayPoints;
+}
--- ALL THAT GLOWS/Assets/Scripts/SpawnerOfEnemiesScript.cs
+++ ALL THAT GLOWS/Assets/Scripts/SpawnerOfEnemiesScript.cs
@@ -5,58 +5,99 @@ using UnityEngine;
 public class SpawnerOfEnemiesScript : MonoBehaviour
 {
     //.....................................Variables
+
     private float timer = 0;
+
+        [Header ("Default Spawn Value (may remove)")]
     public float spawnRate = 5f;
+
+        [Header ("Put models to spawn here")]
     public GameObject normalEnemyPrefab;
     public GameObject lightningEnemyPrefab;
     public GameObject fireEnemyPrefab;
     public GameObject iceEnemyPrefab;
+
     private int enemyType = 0;
     private int enemyAmount = 0;
-    
+
+        [Header("GameObjects with 'EnemyMovementPaths' go here")]
+        [Tooltip("This one needs a GameObject with the 'EnemyMovementPath' Script attached.")]
+    public GameObject[] enemyMovementPathsObjects;
+
+        [Header ("These control waves of enemies.")]
+        [Tooltip ("0=neutral; 1=lightning; 2=fire; 3=ice.")]
+    public int[] waveEnemyElementToSpawn;
+        [Tooltip ("Choose which path that you added above to use. Starts at 0.")]
+    public int[] waveRouteToTake;
+        [Tooltip("How many in this wave of this type with this route to spawn")]
+    public int[] waveNumberOfEnemies;
+        [Tooltip("Delay before spawning this wave")]
+    public float[] waveDelayBeforeWave;
+        [Tooltip("The delay between spawning inside a wave of enemies.")]
+    public float delayInsideWave = .5f;
+
+    private int indexEnemyElement = 0;
+    private int indexRouteToTake = 0;
+    private int indexNumberOfEnemies = 0;
+    private int indexDelayBeforeWaves = 0;
+    private int enemyAmountInWave;
+    private bool waveIsSpawning = false;
+
+    private void Start()
+    {
+        
+    }
+
     void Update()
     {
         //track time
         timer = timer + Time.deltaTime;
 
-        //....................................Spawn at time
-        if (timer >= spawnRate)
+        //....................................Spawn wave at delay time
+        if (timer >= waveDelayBeforeWave[indexDelayBeforeWaves] && waveIsSpawning == false)
         {
             timer = 0;
+            waveIsSpawning = true;
+        }
 
-            if (enemyType == 3)
+        //....................................Spawn enemy inside wave at delay time
+        if (timer >= delayInsideWave && waveIsSpawning == true)
+        {
+            timer = 0;
+            enemyAmount++;
+            enemyAmountInWave++;
+
+            //....................................Spawn enemy of type
+            if (waveEnemyElementToSpawn[indexEnemyElement] == 0)
             {
-                Instantiate(iceEnemyPrefab, gameObject.transform.position, gameObject.transform.rotation);
-                enemyAmount ++;
-            }
+                GameObject newEnemy = Instantiate(normalEnemyPrefab, gameObject.transform.position, gameObject.transform.rotation);
 
-            if (enemyType == 2)
-            {
-                Instantiate(fireEnemyPrefab, gameObject.transform.position, gameObject.transform.rotation);
-                enemyAmount ++;
             }
-
-            if (enemyType == 1)
+            if (waveEnemyElementToSpawn[indexEnemyElement] == 1)
             {
                 Instantiate(lightningEnemyPrefab, gameObject.transform.position, gameObject.transform.rotation);
-                enemyAmount ++;
+            }
+            if (waveEnemyElementToSpawn[indexEnemyElement] == 2)
+            {
+                Instantiate(fireEnemyPrefab, gameObject.transform.position, gameObject.transform.rotation);
+            }
+            if (waveEnemyElementToSpawn[indexEnemyElement] == 3)
+            {
+                Instantiate(iceEnemyPrefab, gameObject.transform.position, gameObject.transform.rotation);
             }
 
-            if (enemyType == 0)
-            {
-                Instantiate(normalEnemyPrefab, gameObject.transform.position, gameObject.transform.rotation);
-                enemyAmount ++;
-            }
 
-            enemyType += 1;
+            //....................................Turn off wave when limit reached, go to next wave in arrays
+            if (enemyAmountInWave >= waveNumberOfEnemies[indexNumberOfEnemies])
+            {
+                enemyAmountInWave = 0;
 
-            if (enemyType == 4)
-            {
-                enemyType = 0;
-            }
-            if (enemyAmount == 10)
-            {
-                Destroy(gameObject);
+                waveIsSpawning = false;
+
+                indexDelayBeforeWaves++;
+                indexEnemyElement++;
+                indexNumberOfEnemies++;
+                indexRouteToTake++;
             }
         }
     }
