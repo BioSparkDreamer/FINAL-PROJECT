7ae29cf3d79fa8102f2529464eef17180084793c
BentNai1 <eattopramen@hotmail.com>
2021-04-20 22:10:04 -0400

    Progress, still need to fix waypoints

--- ALL THAT GLOWS/Assets/Scripts/Enemy Scripts/SingleWaveObject.cs
+++ ALL THAT GLOWS/Assets/Scripts/Enemy Scripts/SingleWaveObject.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class SingleWaveObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Tooltip("Choose how long to wait before spawning this wave.")]
+    public float delayBeforeThisWave;
+
+    [Header("These control mini-waves of enemies.")]
+    [Tooltip("0=neutral; 1=lightning; 2=fire; 3=ice.")]
+    public int[] waveEnemyElementToSpawn;
+    [Tooltip("Choose which spawner to create this mini-wave at in the Spawn/Wave Controller. Starts at 0.")]
+    public int[] waveSpawnerToUse;
+    [Tooltip("Choose which path in the Spawn/Wave Controller this mini-wave should take. Starts at 0.")]
+    public int[] waveRouteToTake;
+    [Tooltip("How many in this mini-wave of this type with this route to spawn. Also determines how many waves there are.")]
+    public int[] waveNumberOfEnemies;
+    [Tooltip("Delay before spawning this mini-wave")]
+    public float[] waveDelayBeforeWave;
+    [Tooltip("The delay between spawning inside a wave of enemies.")]
+    public float delayInsideWave = .5f;
+
     void Start()
     {
         
