100200338b5ce8cc8d47e94053eae293dfe44dfb
Nathan Moyer <49211022+ncmoyer99@users.noreply.github.com>
2021-04-24 20:26:33 -0400

    Mouse state on menus

---
+++ ALL THAT GLOWS/Assets/Scripts/UI Scripts/MouseState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseState : MonoBehaviour
+{
+    public CursorLockMode _cursorModeOnStart;
+
+	public void Start()
+	{
+		Cursor.visible = true;
+		Cursor.lockState = _cursorModeOnStart;
+	}
+}
