56b6438eea7351b4a373f02368e710a0669c5462
BentNai1 <eattopramen@hotmail.com>
2021-04-24 20:13:41 -0400

    Special mode fixed

---
+++ ALL THAT GLOWS/Assets/Scripts/Player Scripts/WeaponSwitch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    [Header("This script also controls the staff models (put the models here)")]
+    public GameObject fire;
+    public GameObject ice;
+    public GameObject lightning;
+
+    public GameObject elementChangerObject;
+    private PlayerWeaponFire elementChanger;
+
+    void Start()
+    {
+        elementChanger = elementChangerObject.GetComponent<PlayerWeaponFire>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            lightning.SetActive(true);
+            fire.SetActive(false);
+            ice.SetActive(false);
+
+            elementChanger.ChangeElement(1);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            lightning.SetActive(false);
+            fire.SetActive(true);
+            ice.SetActive(false);
+
+            elementChanger.ChangeElement(2);
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            lightning.SetActive(false);
+            fire.SetActive(false);
+            ice.SetActive(true);
+
+            elementChanger.ChangeElement(3);
+        }
+    }
+}
